cba08587f06cce13273bb15d636a388ae6e0328c
ripal9755 <ripal9755@gmail.com>
2025-05-30 10:04:31 -0400

    Syntax changed

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-// this is to test
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Live Chat</title>
+    <title>Live Chat Application</title>
     <style>
     #chat-box {
         height: 300px;
